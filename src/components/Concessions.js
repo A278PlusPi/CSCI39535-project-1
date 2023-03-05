c4722cb054834d889b7321e5660811899fad6245
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-03-05 16:20:01 -0500

    Done with the "Concessions" page

--- src/components/Concessions.js
+++ src/components/Concessions.js
@@ -1,4 +1,4 @@
-import './Info.css';
+import './Concessions.css';
 import CottonCandy from './images/concession-cotton-candy-cart.webp';
 import Hat from './images/concession-hat-emporium.webp';
 import IceCream from './images/concession-ice-cream-stand.webp';
@@ -9,7 +9,111 @@ import WDW50 from './images/concession-wdw-50-anniversary-hat-stand.webp';
 function Concessions() {
   return (
     <div>
-    Concessions
+       <div class = "Main-Content">
+
+        <div class = "Card-Series">
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              Cotton Candy Cart
+            </div>
+            <img
+              class="Info-Images"
+              src={CottonCandy} 
+              alt="Cotton Candy Cart"
+              ></img>
+
+            <div class = "Caption">
+            Produces: 53 magic / 6 hours
+            <br></br>
+            Rarity: Rare (3 stars)
+            </div>
+          </div>
+
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              Hat Emporium
+            </div>
+            <img
+              class="Info-Images"
+              src={Hat} 
+              alt="Hat Emporium"
+              ></img>
+              
+            <div class = "Caption">
+            Produces: 72 magic / 12 hours
+            <br></br>
+            Rarity: Uncommon (2 stars)
+            </div>
+          </div>
+
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              Ice Cream Stand
+            </div>
+            <img
+              class="Info-Images"
+              src={IceCream} 
+              alt="Ice Cream Stand"
+              ></img>
+
+            <div class = "Caption">
+            Produces: 60 magic / 12 hours
+            <br></br>
+            Rarity: Common (1 star)
+            </div>
+          </div>
+
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              Mickey's Candy Apple Stand
+            </div>
+            <img
+              class="Info-Images"
+              src={CandyApple} 
+              alt="Mickey's Candy Apple Stand"
+              ></img>
+
+            <div class = "Caption">
+            Produces: 53 magic / 6 hours
+            <br></br>
+            Rarity: Rare (3 stars)
+            </div>
+          </div>
+
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              Popcorn Cart
+            </div>
+            <img
+              class="Info-Images"
+              src={Popcorn} 
+              alt="Popcorn Cart"
+              ></img>
+            <div class = "Caption">
+            Produces: 53 magic / 8 hours
+            <br></br>
+            Rarity: Uncommon (2 stars)
+            </div>
+          </div>
+
+          <div class = "Info-Card">
+            <div class = "H1-Text">
+              WDW 50th Anniversary Hat Stand
+            </div>
+            <img
+              class="Info-Images"
+              src={WDW50} 
+              alt="WDW 50th Anniversary Hat Stand"
+              ></img>
+            <div class = "Caption">
+            Produces: 61 magic / 6 hours
+            <br></br>
+            Rarity: Epic (4 stars)
+            </div>
+          </div>
+        </div>
+        
+       </div>
     </div>
   );
 }
